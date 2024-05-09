c9e359ddd7d510c652c18d6b38506a0cfee2196a
Ramon Nieto <ramon.nieto@gmail.com>
2024-05-09 23:38:30 +0200

    update

--- prueba.py
+++ prueba.py
@@ -1,15 +1,25 @@
+#import pyrebase
+
+# Configuración de Firebase
+# config = {
+#     "apiKey": "TU_API_KEY",
+#     "authDomain": "TU_DOMINIO.firebaseapp.com",
+#     "databaseURL": "https://TU_DOMINIO.firebaseio.com",
+#     "storageBucket": "TU_DOMINIO.appspot.com"
+# }
+# firebase = pyrebase.initialize_app(config)
+# db = firebase.database()
+#firebase-adminsdk-cgdgv@my-first-project---prueba.iam.gserviceaccount.com	firebase-adminsdk	
+#Correo electrónico
+#firebase-adminsdk-cgdgv@my-first-project---prueba.iam.gserviceaccount.com
+#ID único
+#113756081115723530801
 print("---------------------------")
 print("Bienvenido a Clinica PePe")
 print("---------------------------")
 tipo_perfil=""  # iniciamos variable
 while tipo_perfil != "client" and tipo_perfil != "doctor": # bucle para definir un perfil 
     tipo_perfil=input("Que perfil ejecutar un valor client o doctor: ")
-"""     if tipo_perfil == "client":
-        print("Hola soy un cliente")
-    elif tipo_perfil =="doctor":
-        print("Hola soy el doctor")
-    else:
-        print("seleccione un valor client o doctor") """
 
 #print("Estoy fuera")
 
@@ -22,11 +32,16 @@ if tipo_perfil == "client":
     client["apellido_client"]=input("Dame un apellido: ")
     client["telf_client"]=input("Dame un telf: ")
     print(client)
-# La variable diccionario subirla a la bbdd
-#
+    datos_client=client
+# la variable client se puede guardar en un archivo o en la bbdd
 # perfil doctor
-#
+
 else:
     # Se implementa espacio doctor
     print("Hola soy el doctor")
+# Qué puede hacer el doctor ? # ver los datos del cliente
+# No se puede acceder al diccionario client pq esta guardado
 
+
+# Registramos en la bbdd los datos de cliente
+# db.child("clientes").push(client)
